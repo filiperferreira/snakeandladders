f90deb877d52ec5a9c36a3e436ba27f26a2a4ccb
Filipe Ferreira <frferreira9@gmail.com>
2017-09-03 14:29:40 -0300

    new stuff

--- main.cc
+++ main.cc
@@ -4,6 +4,8 @@
 #include <random>
 #include <chrono>
 
+#define N_PLAYERS 4
+
 using namespace std;
 
 unsigned seed = chrono::system_clock::now().time_since_epoch().count();
@@ -12,10 +14,12 @@ sf::Font font;
 
 class GameLogic {
     int curPlayer;
+    int state;
 
     public:
     GameLogic() {
         curPlayer = 0;
+        state = 0;
     }
 
     int getCurPlayer() {
@@ -24,26 +28,35 @@ class GameLogic {
 
     void nextPlayer() {
         curPlayer++;
-        if (curPlayer == 4) {
+        if (curPlayer == N_PLAYERS) {
             curPlayer = 0;
         }
     }
+
+    int getState() {
+        return state;
+    }
+
+    void setState(int value) {
+        state = value;
+    } 
 };
 
 class Square {
     int xPos, yPos, number;
-    int leadsTo;
+    int leadsTo, gold;
     sf::RectangleShape square;
-    sf::Text numberText;
+    sf::Text numberText, goldText;
 
     public:
     Square() {}
     Square(int x, int y, int n): xPos(x), yPos(y), number(n) {
+        gold = 5;
         leadsTo = -1;
 
         square = sf::RectangleShape(sf::Vector2f(63, 63));
         square.setPosition(x, y);
-        square.setFillColor(sf::Color(255,255,255));
+        square.setFillColor(sf::Color(139,69,19));
         square.setOutlineThickness(1);
         square.setOutlineColor(sf::Color(0,0,0));
 
@@ -52,6 +65,12 @@ class Square {
         numberText.setCharacterSize(10);
         numberText.setColor(sf::Color(0,0,0));
         numberText.setPosition(x+2, y+2);
+
+        goldText.setFont(font);
+        goldText.setString(to_string(gold));
+        goldText.setCharacterSize(18);
+        goldText.setColor(sf::Color::Yellow);
+        goldText.setPosition(x+25,y+19);
     }
 
     sf::RectangleShape getShape() {
@@ -61,6 +80,8 @@ class Square {
     void drawSquare(sf::RenderWindow& window) {
         window.draw(square);
         window.draw(numberText);
+        goldText.setString(to_string(gold));
+        window.draw(goldText);
     }
 
     void setLeadsTo(int d) {
@@ -77,6 +98,14 @@ class Square {
 
     int getPosY() {
         return yPos;
+    }
+
+    int getGold() {
+        return gold;
+    }
+
+    void setGold(int value) {
+        gold = value;
     }
 };
 
@@ -133,7 +162,7 @@ class Board {
         square[97].setLeadsTo(77);
     }
 
-    Square getSquare(int i) {
+    Square& getSquare(int i) {
         return square[i];
     }
 
@@ -162,13 +191,15 @@ class Board {
 
 class Player {
     int xOffset, yOffset;
-    int squarePos;
+    int squarePos, gold, diamonds;
     sf::CircleShape token;
 
     public:
     Player() {};
     Player(Board b, int p) {
         squarePos = 0;
+        gold = 0;
+        diamonds = 0;
 
         token = sf::CircleShape(16);
 
@@ -201,7 +232,7 @@ class Player {
         return token;
     }
 
-    void move(int value, Board b) {
+    void move(int value, Board& b, GameLogic& g) {
         int mult = 1;
 
         while (value > 0) {
@@ -213,22 +244,38 @@ class Player {
             }
         }
 
-        if (b.getSquare(squarePos).getLeadsTo() != -1) {
-            squarePos = b.getSquare(squarePos).getLeadsTo();
+        if (b.getSquare(squarePos).getGold() > 0) {
+            b.getSquare(squarePos).setGold(b.getSquare(squarePos).getGold() - 1);
+            gold++;
         }
 
         token.setPosition(b.getSquare(squarePos).getPosX() + xOffset, b.getSquare(squarePos).getPosY() + yOffset);
 
-        /*if (squarePos == 99) {
-            sf::Text youWin;
-            sf::Font winFont;
+        if (b.getSquare(squarePos).getLeadsTo() != -1) {
+            if (b.getSquare(squarePos).getLeadsTo() < squarePos) {
+                if (diamonds > 0) {
+                    g.setState(1);
+                }
+                else {
+                    squarePos = b.getSquare(squarePos).getLeadsTo();
+                    g.nextPlayer();
+                }
+            }
+            else {
+                g.setState(2);
+            }
+        }
+        else {
+            g.nextPlayer();
+        }
+    }
 
-            winFont.loadFromFile("Verdana.ttf");
-            youWin.setFont(winFont);
-            youWin.setString("You Win!");
-            youWin.setCharacterSize(50);
-            youWin.setColor(token.getFillColor());
-        }*/
+    int getGold() {
+        return gold;
+    }
+
+    int getDiamonds() {
+        return diamonds;
     }
 };
 
@@ -236,7 +283,7 @@ class Dice {
     int value;
     sf::Text valueText, rollText;
     sf::RectangleShape button;
-    std::uniform_int_distribution<int> distribution;
+    uniform_int_distribution<int> distribution;
 
     public:
     Dice() {
@@ -279,8 +326,87 @@ class Dice {
             this->rollDice();
         }
 
-        player.move(value, board);
-        game.nextPlayer();
+        player.move(value, board, game);
+        //game.nextPlayer();
+    }
+};
+
+class LadderMenu {
+    sf::Text acceptText, refuseText;
+    sf::RectangleShape button1, button2;
+
+    public:
+    LadderMenu() {
+        button1 = sf::RectangleShape(sf::Vector2f(125,39));
+        button1.setPosition(640 + 15, 530);
+        button1.setFillColor(sf::Color(255,255,255));
+
+        button2 = sf::RectangleShape(sf::Vector2f(125,39));
+        button2.setPosition(640 + 15, 580);
+        button2.setFillColor(sf::Color(255,255,255));
+
+        acceptText.setFont(font);
+        acceptText.setCharacterSize(15);
+        acceptText.setColor(sf::Color(0,0,0));
+        acceptText.setPosition(640 + 25, 585);
+        acceptText.setString("Get Diamond");
+
+        refuseText.setFont(font);
+        refuseText.setCharacterSize(15);
+        refuseText.setColor(sf::Color(0,0,0));
+        refuseText.setPosition(640 + 25, 535);
+        refuseText.setString("Go Upstairs");
+    }
+
+    void drawMenu(sf::RenderWindow& window) {
+        window.draw(button1);
+        window.draw(button2);
+        window.draw(acceptText);
+        window.draw(refuseText);
+    }
+};
+
+class Leaderboard {
+    vector<sf::CircleShape> players;
+    vector<sf::Text> goldText, diamondText;
+
+    public:
+    Leaderboard(int n, vector<Player> p): players(vector<sf::CircleShape>(n)), goldText(vector<sf::Text>(n)), diamondText(vector<sf::Text>(n)) {
+        int x = 655;
+        int y = 180;
+
+        for (int i = 0; i < n; i++) {
+            players[i] = p[i].getShape();
+            players[i].setPosition(x, y + 50 * i);
+
+            goldText[i].setFont(font);
+            goldText[i].setString("0");
+            goldText[i].setCharacterSize(25);
+            goldText[i].setColor(sf::Color::Yellow);
+            goldText[i].setPosition(x + 50, y + 50 * i);
+
+            diamondText[i].setFont(font);
+            diamondText[i].setString("0");
+            diamondText[i].setCharacterSize(25);
+            diamondText[i].setColor(sf::Color(0,255,255));
+            diamondText[i].setPosition(x + 100, y + 50 * i);
+        }
+    }
+
+    void drawLeaderboard(sf::RenderWindow& window, vector<Player> p, GameLogic g) {
+        for (int i = 0; i < N_PLAYERS; i++) {
+            players[i].setOutlineThickness(0);
+            if (g.getCurPlayer() == i) {
+                players[i].setOutlineThickness(2);
+            }
+            window.draw(players[i]);
+
+            goldText[i].setString(to_string(p[i].getGold()));
+            window.draw(goldText[i]);
+
+            diamondText[i].setString(to_string(p[i].getDiamonds()));
+            window.draw(diamondText[i]);
+        }
     }
 };
 
@@ -290,13 +416,16 @@ int main() {
     
     GameLogic game;
     Board board(100);
-    vector<Player> players(4);
+    vector<Player> players(N_PLAYERS);
     Dice dice;
 
     board.initBoard();
-    for (int i = 0; i < 4; i++) {
+    for (int i = 0; i < N_PLAYERS; i++) {
         players[i] = Player(board, i+1);
     }
+
+    Leaderboard leaderboard(N_PLAYERS, players);
+    LadderMenu menu;
 
     while (window.isOpen()) {
         sf::Event event;
@@ -305,8 +434,10 @@ int main() {
                 window.close();
             }
             if (event.type == sf::Event::MouseButtonPressed) {
-                if (event.mouseButton.button == sf::Mouse::Left) {
-                    dice.checkDiceRoll(window, players[game.getCurPlayer()], board, game);
+                if (game.getState() == 0) {
+                    if (event.mouseButton.button == sf::Mouse::Left) {
+                        dice.checkDiceRoll(window, players[game.getCurPlayer()], board, game);
+                    }
                 }
             }
         }
@@ -317,10 +448,12 @@ int main() {
             board.getSquare(i).drawSquare(window);
         }
         board.drawStairsAndSnakes(window);
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < N_PLAYERS; i++) {
             window.draw(players[i].getShape());
         }
         dice.drawDice(window);
+        leaderboard.drawLeaderboard(window, players, game);
+        menu.drawMenu(window);
 
         window.display();
     }
